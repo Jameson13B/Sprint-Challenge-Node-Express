95f28cea706096a95a578d2871c36831381e3f61
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-07-27 10:57:24 -0600

    Created .get:id endpoints for projects and actions

--- data/routes/actionRoute.js
+++ data/routes/actionRoute.js
@@ -7,5 +7,14 @@ router.get('/', (req, res) => {
         .then(response => res.status(200).json(response))
         .catch(err => res.status(500).json({ error: "Action .get catch error" }))
 })
+router.get('/:id', (req, res) => {
+    actionModel.get(req.params.id)
+        .then(response => {
+            if (!response) {
+                res.status(400).json({ message: "The action with the ID does not exist." })
+            } res.status(200).json(response)
+        })
+        .catch(err => res.status(500).json({ error: "The action information could not be retrieved." }))
+})
 
 module.exports = router;
--- data/routes/projectRoute.js
+++ data/routes/projectRoute.js
@@ -5,7 +5,16 @@ const router = express.Router();
 router.get('/', (req, res) => {
     projectModel.get()
         .then(response => res.status(200).json(response))
-        .catch(err => res.status(500).json({ error: "Project .get catch error" }))
+        .catch(err => res.status(500).json({ error: "The projects information could not be retrieved." }))
+})
+router.get('/:id', (req, res) => {
+    projectModel.get(req.params.id)
+        .then(response => {
+            if (!response) {
+                res.status(400).json({ message: "The project with the ID does not exist." })
+            } res.status(200).json(response)
+        })
+        .catch(err => res.status(500).json({ error: "The project information could not be retrieved." }))
 })
 
 module.exports = router;
