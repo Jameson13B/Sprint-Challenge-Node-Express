f20c6b2748046664e8613f714795affd2a3cecd0
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-07-27 13:01:34 -0600

    A list of projects now rendfers on the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
-
+ 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+  componentWillMount() {
+    axios
+      .get('http://localhost:8000/projects')
+      .then(response => {
+        console.log('GET RESPONSE: ', response)
+        this.setState({ projects: response.data })
+      })
+      .catch(err => {console.log(err)})
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +26,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {this.state.projects.map(project => {
+            return (
+              <div>
+                <h1>{project.name}</h1>
+              </div>
+            );
+          })}
+        </ul>
       </div>
     );
   }
--- data/routes/projectRoute.js
+++ data/routes/projectRoute.js
@@ -3,53 +3,62 @@ const projectModel = require('../helpers/projectModel');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    projectModel.get()
-        .then(response => res.status(200).json(response))
-        .catch(err => res.status(500).json({ error: "The projects information could not be retrieved." }))
+  projectModel.get()
+    .then(response => res.status(200).json(response))
+    .catch(err => res.status(500).json({ error: "The projects information could not be retrieved." }))
 })
 router.get('/:id', (req, res) => {
-    projectModel.get(req.params.id)
-        .then(response => {
-            if (!response) {
-                res.status(400).json({ message: "The project with the ID does not exist." })
-            } res.status(200).json(response)
-        })
-        .catch(err => res.status(500).json({ error: "The project information could not be retrieved." }))
+  projectModel.get(req.params.id)
+    .then(response => {
+      if (!response) {
+        res.status(400).json({ message: "The project with the ID does not exist." })
+      } res.status(200).json(response)
+    })
+    .catch(err => res.status(500).json({ error: "The project information could not be retrieved." }))
+})
+router.get('/:id/actions', (req, res) => {
+  projectModel.getProjectActions(req.params.id)
+    .then(response => {
+      if (!response) {
+        res.status(404).json({ message: "The project with the ID does not exist." })
+      } res.status(200).json(response);
+    })
+    .catch(err => res.status(500).json({ error: "The project's actions could not be retrieved."}))
 })
 router.post('/', (req, res) => {
-    const { name, description, completed } = req.body;
-    if (!name || !description) {
-        res.status(400).json({ errorMessage: "Please provide a name and description for the project." })
-    }
-    projectModel.insert({ name, description, completed })
-        .then(response => res.status(201).json(response))
-        .catch(err => res.status(500).json({ error: "The project could not be saved to the database" }))
+  const { name, description, completed } = req.body;
+  if (!name || !description) {
+    res.status(400).json({ errorMessage: "Please provide a name and description for the project." })
+  }
+  projectModel.insert({ name, description, completed })
+    .then(response => res.status(201).json(response))
+    .catch(err => res.status(500).json({ error: "The project could not be saved to the database" }))
 })
 router.delete('/:id', (req, res) => {
-    projectModel.remove(req.params.id)
-        .then(response => {
-            if (!response) {
-                res.status(404).json({ message: "The project with the ID does not exist." })
-            } else {
-                res.status(200).json({ message: "The project has been deleted."})
-            }
-        })
-        .catch(err => res.status(500).json({ error: "The project could not be removed." }))
+  projectModel.remove(req.params.id)
+    .then(response => {
+      if (!response) {
+        res.status(404).json({ message: "The project with the ID does not exist." })
+      } else {
+        res.status(200).json({ message: "The project has been deleted." })
+      }
+    })
+    .catch(err => res.status(500).json({ error: "The project could not be removed." }))
 })
 router.put('/:id', (req, res) => {
-    const { name, description, completed } = req.body;
-    if (!name || !description ) {
-        res.status(400).json({ errorMessage: "Please provide name and description for the project." })
-    }
-    projectModel.update(req.params.id, { name, description, completed })
-        .then(response => {
-            if (response === null) {
-                res.status(404).json({ message: "The project with the specified ID does not exist." })
-            } else {
-                res.status(200).json({ response });
-            }
-        })
-        .catch(err => res.status(500).json({ error: "The project could not be updated" }))
+  const { name, description, completed } = req.body;
+  if (!name || !description) {
+    res.status(400).json({ errorMessage: "Please provide name and description for the project." })
+  }
+  projectModel.update(req.params.id, { name, description, completed })
+    .then(response => {
+      if (response === null) {
+        res.status(404).json({ message: "The project with the specified ID does not exist." })
+      } else {
+        res.status(200).json({ response });
+      }
+    })
+    .catch(err => res.status(500).json({ error: "The project could not be updated" }))
 })
 
 module.exports = router;
